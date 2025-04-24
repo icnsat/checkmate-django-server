27941b9a1db90f70523d9d88e2ba9a93eb13e4e8
Elizaveta Gornovova <l.gornovova@yandex.ru>
2025-04-24 14:30:50 +0300

    added migration output

--- build_files.sh
+++ build_files.sh
@@ -4,8 +4,8 @@ echo "Build packages"
 python3 -m pip install -r requirements.txt
 
 echo "Migrating database"
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 echo "Collecting static files"
 python3 manage.py collectstatic --noinput
